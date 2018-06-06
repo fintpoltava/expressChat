00f5af4a09a9e5716fe01153724e8bd0b7ce6181
Viktor Fursov <vfursov@corevalue.net>
2018-06-06 14:05:49 +0300

    fix flash

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var passportRouter = require('./routes/passport');
 
-var db = require('./db');
+var db = require('./db')
+var flash = require('connect-flash')
 
 var app = express();
 // view engine setup
@@ -21,15 +22,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // Socket.io
-var io           = socket_io();
-app.io           = io;
+var io = socket_io()
+app.io = io
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'mySecretKey'}));
+app.use(flash())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({secret: 'mySecretKey'}))
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
